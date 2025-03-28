c8dc9a9e7e374610f4c98d8e1d8f62209701a374
remoteguidesystem <remoteguidesystem@gmail.com>
2025-03-28 22:21:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,10 +25,11 @@ async function getLatestImageUrl() {
     let imageUrl = "https://remoteguidesystem.github.io/grafana-image/yaba101.png";
     for (let line of lines) {
         const columns = line.split(",");
-        if (columns.length > 7 && columns[7] === "locID") {
-            imageUrl = "https://remoteguidesystem.github.io/grafana-image/yaba" & columns[6] & ".png";
+        if (columns.length > 6 && columns[6] === "locID") {
+            imageUrl = "https://remoteguidesystem.github.io/grafana-image/yaba" & columns[7] & ".png";
         }
     }
+        console.log("Image URL:", imageUrl);
     return imageUrl;
 }
 
