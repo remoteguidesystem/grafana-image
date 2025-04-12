365b735555d50453a9558ec94da6fce3065bf01e
remoteguidesystem <remoteguidesystem@gmail.com>
2025-04-12 17:19:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ async function getLatestImageUrl() {
         const columns = line.split(",");
         console.log("columns[6] locID :", columns[6]);
         if (columns.length > 6 && columns[7] === "locID") {
-            imageUrl = "https://remoteguidesystem.github.io/grafana-image/yaba" + columns[6] + ".png";
+            imageUrl = "https://remotoguidesystem.netlify.app/images/yaba" + columns[6] + ".jpg";
         }
     }
         console.log("Image URL:", imageUrl);
